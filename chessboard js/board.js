24ce0f5da2671ef1e7487f767aa896986e1baa9f
unknown <danyyyyy223@gmail.com>
2022-04-20 01:23:57 +0300

    Added collisions and possible eat enemy moves

--- chessboard js/board.js
+++ chessboard js/board.js
@@ -51,6 +51,12 @@ function knightPaintPossibleMovement(row, col, color){
         x = row + xArray[i];
         y = col + yArray[i];
         if(isPointInBounds(x, y)){
+            if(pieces[x][y] && pieces[x][y].player !== color) {
+                paintPossibleEnemyMove(x,y);
+                continue;
+            } 
+            else if(pieces[x][y] && pieces[x][y].player === color) continue;
+            //else if cell is empty
         paintPossibleMove(x,y);
         }
     }
@@ -64,10 +70,16 @@ function bishopPaintPossibleMovement(row, col, color){
         x = row + xArray[i];
         y = col + yArray[i];
         for(let j = 0; j < TABLE_SIZE; j++){
-            if (!isPointInBounds(x, y)) break;    
-                paintPossibleMove(x,y);
-                x += xArray[i];
-                y += yArray[i];
+            if (!isPointInBounds(x, y)) break;  
+            if(pieces[x][y] && pieces[x][y].player !== color) {
+                paintPossibleEnemyMove(x,y);
+                break;
+            } 
+            else if(pieces[x][y] && pieces[x][y].player === color) break;  
+            //else if the cell is empty
+            paintPossibleMove(x,y);
+            x += xArray[i];
+            y += yArray[i];
         }
     }
 }
@@ -81,45 +93,74 @@ function rookPaintPossibleMovement(row, col, color){
         x = row + xArray[i];
         y = col + yArray[i];
         for(let j = 0; j < TABLE_SIZE; j++){
-            if (!isPointInBounds(x, y)) break;    
-                paintPossibleMove(x,y);
-                x += xArray[i];
-                y += yArray[i];
+            if (!isPointInBounds(x, y)) break;   
+            if(pieces[x][y] && pieces[x][y].player !== color) {
+                paintPossibleEnemyMove(x,y);
+                break;
+            } 
+            else if(pieces[x][y] && pieces[x][y].player === color) break;
+            //else if the cell is empty
+            paintPossibleMove(x,y);
+            x += xArray[i];
+            y += yArray[i];
         }
     }
 }
 
-function kingPaintPossibleMovement(row, col){
+function kingPaintPossibleMovement(row, col, color){
     //all king possible moves in this 2 arrays
     let xArray = [ 1, -1, 0, 0, 1, -1, 1, -1];
     let yArray = [ 0, 0, -1, 1, 1, -1, -1, 1];
     for(let i = 0; i < xArray.length; i++){
         x = row + xArray[i];
         y = col + yArray[i];
-        if(isPointInBounds(x, y))
+        if(isPointInBounds(x, y)){
+            if(pieces[x][y] && pieces[x][y].player !== color) {
+                paintPossibleEnemyMove(x,y);
+                continue;
+            } 
+            else if(pieces[x][y] && pieces[x][y].player === color) continue;
+            //else if cell is empty
+        }
         paintPossibleMove(x,y);
+    }
+}
+
+
+
+function queenPaintPossibleMovement(row, col, color){
+    //queen combines the movement of the rook and the bishop :o
+    rookPaintPossibleMovement(row, col, color);
+    bishopPaintPossibleMovement(row, col, color);
+}
+
+function pawnPaintPossibleMovement(row, col, color){
+    if(color === WHITE_PLAYER){
+        if(pieces[row + 1][col + 1] && pieces[row + 1][col + 1].player !== color ){
+            paintPossibleEnemyMove(row + 1,col + 1);
+        }
+        if(pieces[row + 1][col - 1] && pieces[row + 1][col - 1].player !== color ){
+            paintPossibleEnemyMove(row + 1,col - 1);
+        }
+        if(pieces[row + 1][col] === undefined )
+        paintPossibleMove(row + 1, col);
+    }
+    //else if the pawn is black
+    else{
+        if(pieces[row - 1][col - 1] && pieces[row - 1][col - 1].player !== color ){
+            paintPossibleEnemyMove(row - 1,col - 1);
+        }
+        if(pieces[row - 1][col + 1] && pieces[row - 1][col + 1].player !== color ){
+            paintPossibleEnemyMove(row - 1,col + 1);
+        }
+        if(pieces[row - 1][col] === undefined )
+        paintPossibleMove(row - 1, col);
     }
 }
 
 function isPointInBounds(x, y){
     return (x >= 0 && y >= 0 && x < TABLE_SIZE && y < TABLE_SIZE);
 }
-
-function queenPaintPossibleMovement(row, col){
-    //queen combines the movement of the rook and the bishop :o
-    rookPaintPossibleMovement(row, col);
-    bishopPaintPossibleMovement(row, col);
-}
-
-function pawnPaintPossibleMovement(row, col, color){
-    if(color === WHITE_PLAYER){
-        paintPossibleMove(row+1, col);
-    }
-    else{
-        paintPossibleMove(row-1, col);;
-    }
-}
-
 
 function addImage(cell, player, name) {
     const image = document.createElement('img');
@@ -173,6 +214,7 @@ function onCellClick(e) {
     for (let i = 0; i < TABLE_SIZE; i++) {
         for (let j = 0; j < TABLE_SIZE; j++) {
           htmlTable.rows[i].cells[j].classList.remove('possibleMove');
+          htmlTable.rows[i].cells[j].classList.remove('possibleEat');
         }
     }
 
@@ -188,30 +230,38 @@ function onCellClick(e) {
         e.currentTarget.classList.add('selected');
         selectedCell = e.currentTarget;
     }
-
+    if(selectedCell !== undefined){
+    
     let currentPiece = pieces[selectedCell.parentNode.rowIndex][selectedCell.cellIndex];
-    if(currentPiece.type === 'rook'){
-        rookPaintPossibleMovement(selectedCell.parentNode.rowIndex, selectedCell.cellIndex,);
+    if(currentPiece === undefined){
+        return;
+    }
+    else if(currentPiece.type === 'rook'){
+        rookPaintPossibleMovement(selectedCell.parentNode.rowIndex, selectedCell.cellIndex, currentPiece.player);
     }
     else if(currentPiece.type === 'pawn'){
         pawnPaintPossibleMovement(selectedCell.parentNode.rowIndex, selectedCell.cellIndex, currentPiece.player);
     }
     else if(currentPiece.type === 'knight'){
-        knightPaintPossibleMovement(selectedCell.parentNode.rowIndex, selectedCell.cellIndex);
+        knightPaintPossibleMovement(selectedCell.parentNode.rowIndex, selectedCell.cellIndex, currentPiece.player);
     }
     else if(currentPiece.type === 'bishop'){
-        bishopPaintPossibleMovement(selectedCell.parentNode.rowIndex, selectedCell.cellIndex);
+        bishopPaintPossibleMovement(selectedCell.parentNode.rowIndex, selectedCell.cellIndex, currentPiece.player);
     }
     else if(currentPiece.type === 'king'){
-        kingPaintPossibleMovement(selectedCell.parentNode.rowIndex, selectedCell.cellIndex);
+        kingPaintPossibleMovement(selectedCell.parentNode.rowIndex, selectedCell.cellIndex, currentPiece.player);
     }
     else if(currentPiece.type === 'queen'){
-        queenPaintPossibleMovement(selectedCell.parentNode.rowIndex, selectedCell.cellIndex);
+        queenPaintPossibleMovement(selectedCell.parentNode.rowIndex, selectedCell.cellIndex, currentPiece.player);
     }   
+}
 
 }
 function paintPossibleMove(row, col){
     htmlTable.rows[row].cells[col].classList.add('possibleMove');
+}
+function paintPossibleEnemyMove(row, col){
+    htmlTable.rows[row].cells[col].classList.add('possibleEat');
 }
 function unpaintPossibleMove(cell){
     cell.currentTarget.classList.remove('selected');
